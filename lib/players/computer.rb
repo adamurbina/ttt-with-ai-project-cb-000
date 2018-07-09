72e794df7639ae0ea9a5beb07fbea89e0a15b0c5
Adam Urbina <adamurbina@gmail.com>
2018-07-09 18:57:38 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -32,14 +32,14 @@ module Players
                     end
                 }
                 combo_moves.each_with_index{ |combo, index|
-                    if combo.count(game.player_2.token) == 0 && combo.count(" ") == 2
+                    if combo.count(game.player_2.token) == 1 && combo.count(" ") == 2
                         inner_index = combo.index(" ")
                         outer_index = index
                         move = game.WIN_COMBINATIONS[outer_index][inner_index]
                     end
                 }
                 combo_moves.each_with_index{ |combo, index|
-                    if combo.count(self.token) == 0 && combo.count(" ") == 3
+                    if combo.count(game.player_2.token) == 0 && combo.count(" ") == 3
                         inner_index = combo.index(" ")
                         outer_index = index
                         move = game.WIN_COMBINATIONS[outer_index][inner_index]
